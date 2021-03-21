f5de0824447e3cbc7b9208f37de831177ab309cd
Jason Schmitt <schmitty890@gmail.com>
2021-03-20 22:25:10 -0400

    separating into controller and routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import emailRoutes from "./routes/emailRoutes";
 // import routes from "./src/routes/crmRoutes";
 // import testRoutes from "./src/routes/testRoutes";
 // import budgetRoutes from "./src/routes/budgetRoutes";
@@ -7,7 +8,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 // import jsonwebtoken from "jsonwebtoken";
-import nodemailer from "nodemailer";
 
 const app = express();
 
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+emailRoutes(app);
 // routes(app);
 // budgetRoutes(app);
 // testRoutes(app);
@@ -31,42 +32,6 @@ app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
   res.send("test route!");
-});
-
-app.post("/test", (req, res) => {
-  console.log(req.body);
-  // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAILEMAIL,
-        pass: process.env.GMAILPASS,
-      },
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: `${req.body.yourEmail}`, // sender address
-      to: process.env.GMAILEMAIL, // list of receivers
-      subject: "Portfolio email", // Subject line
-      text: `A message from ${req.body.yourName} at ${req.body.yourEmail}!  ${req.body.yourMessage}`, // plain text body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
-
-  main().catch(console.error);
 });
 
 app.get("/", (req, res) => {
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,20 +1,9 @@
-// const routes = (app) => {
-//   app
-//     .route("/contact")
-//     // get all contacts
-//     .get(
-//       (req, res, next) => {
-//         // middleware
-//         console.log(`Request from: ${req.originalUrl}`);
-//         console.log(`Request type: ${req.method}`);
-//         next();
-//       },
-//       loginRequired,
-//       getContacts
-//     )
+import { sendEmail } from "../controllers/emailController";
+const routes = (app) => {
+  app
+    .route("/email")
+    // send email to me
+    .post(sendEmail);
+};
 
-//     // add a new contact
-//     .post(loginRequired, addNewContact);
-// };
-
-// export default routes;
+export default routes;
