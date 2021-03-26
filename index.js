b789aaa1554b45dc52de09fcebb8f161dc2dde43
Jason Schmitt <schmitty890@gmail.com>
2021-03-25 23:38:00 -0400

    db test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-import emailRoutes from "./routes/emailRoutes";
-// import routes from "./src/routes/crmRoutes";
+import emailRoutes from "./src/routes/emailRoutes";
+import routes from "./src/routes/crmRoutes";
 // import testRoutes from "./src/routes/testRoutes";
 // import budgetRoutes from "./src/routes/budgetRoutes";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-// import jsonwebtoken from "jsonwebtoken";
+import jsonwebtoken from "jsonwebtoken";
 
 const app = express();
 
@@ -18,12 +18,45 @@ const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081;
 // enable cors
 app.use(cors());
 
+// mongoose connection
+mongoose.Promise = global.Promise;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/jasonsProjectsDB";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // body parser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// JWT setup
+app.use((req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    jsonwebtoken.verify(
+      req.headers.authorization.split(" ")[1],
+      "RESTFULAPIs",
+      (err, decode) => {
+        if (err) {
+          req.user = undefined;
+        }
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
+routes(app);
 emailRoutes(app);
-// routes(app);
 // budgetRoutes(app);
 // testRoutes(app);
 
