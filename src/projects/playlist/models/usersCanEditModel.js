4800977d6371ec51c1817b301e0496b2b6eb28e2
Jason Schmitt <schmitty890@gmail.com>
2021-04-01 16:36:29 -0400

    adding to user can edit playlist model

--- src/projects/playlist/models/usersCanEditModel.js
+++ src/projects/playlist/models/usersCanEditModel.js
@@ -6,6 +6,18 @@ export const UsersCanEditSchema = new Schema({
   canEdit: {
     type: Boolean,
   },
+  canEditPlay: {
+    type: Boolean,
+  },
+  canEditPause: {
+    type: Boolean,
+  },
+  canEditSkip: {
+    type: Boolean,
+  },
+  canEditSearch: {
+    type: Boolean,
+  },
 });
 
 UsersCanEditSchema.set("timestamps", true);
